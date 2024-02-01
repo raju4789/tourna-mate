b4ae88ed22d687d5b14eb80ec3faab48e030c1e8
Raju MLN <narasimha4789@gmail.com>
2024-02-01 08:06:47 +0700

    added authentication service

--- tourni-gateway/src/main/java/com/tournament/filters/AuthenticationFilter.java
+++ tourni-gateway/src/main/java/com/tournament/filters/AuthenticationFilter.java
@@ -8,11 +8,13 @@ import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -36,41 +38,44 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
             if (routeValidator.isSecured.test(exchange.getRequest())) {
                 List<String> authorizationHeaders = exchange.getRequest().getHeaders().get("Authorization");
                 if (authorizationHeaders == null || authorizationHeaders.isEmpty()) {
-                    throw new RuntimeException("Authorization header is missing in request");
+                    return onError(exchange, "Authorization header is missing in request");
                 }
 
                 final String authorizationHeader = authorizationHeaders.get(0);
 
                 if (authorizationHeader == null || !StringUtils.startsWithIgnoreCase(authorizationHeader, "Bearer ")) {
-                    throw new RuntimeException("Authorization header is missing or invalid");
+                    return onError(exchange, "Authorization header is missing or invalid");
                 }
 
                 final String token = authorizationHeader.substring(7);
 
-                try {
-                    AppTokenValidationRequest req = AppTokenValidationRequest.builder().token(token).build();
+                AppTokenValidationRequest req = AppTokenValidationRequest.builder().token(token).build();
 
-                    CommonApiResponse body = webClient.post()
-                            .uri("/api/v1/auth/validateToken")
-                            .bodyValue(BodyInserters.fromValue(req))
-                            .retrieve()
-                            .bodyToMono(CommonApiResponse.class)
-                            .block();
-
-
-                    if (body == null || !body.isSuccess() || body.getData() == null || !body.getData().isValid()) {
-                        throw new RuntimeException("Authorization header is invalid: " +
-                                (body != null ? body.getErrorDetails().getErrorMessage() : "Unknown error"));
-                    }
-
-                } catch (Exception e) {
-                    log.error("Exception while processing authentication: {}", e.getMessage(), e);
-                    throw new RuntimeException("Unknown Authorization header is invalid" +
-                            (e.getMessage() != null ? e.getMessage() : ""));
-                }
+                return webClient.post()
+                        .uri("/api/v1/auth/validateToken")
+                        .bodyValue(BodyInserters.fromValue(req))
+                        .retrieve()
+                        .bodyToMono(new ParameterizedTypeReference<CommonApiResponse<AppTokenValidationResponse>>() {
+                        })
+                        .flatMap(body -> {
+                            if (body == null || !body.isSuccess() || body.getData() == null || !body.getData().isValid()) {
+                                return onError(exchange, "Authorization header is invalid: " +
+                                        (body != null ? body.getErrorDetails().getErrorMessage() : "Unknown error"));
+                            }
+                            return chain.filter(exchange);
+                        })
+                        .onErrorResume(e -> onError(exchange, "Unknown Authorization header is invalid " +
+                                (e.getMessage() != null ? e.getMessage() : "")));
             }
+
             return chain.filter(exchange);
         };
+    }
+
+    private Mono<Void> onError(ServerWebExchange exchange, String errorMessage) {
+        log.error("Exception while processing authentication: {}", errorMessage);
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
     }
 
     public static class Config {
