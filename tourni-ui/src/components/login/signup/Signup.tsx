2f38bab4f5f95caa149bc9088b29de7cd89a7059
Raju MLN <narasimha4789@gmail.com>
2024-02-10 18:26:34 +0700

    added touni-ui

--- tourni-ui/src/components/login/signup/Signup.tsx
+++ tourni-ui/src/components/login/signup/Signup.tsx
@@ -21,7 +21,6 @@ const Signup: React.FC = () => {
   const [confirmPassword, setConfirmPassword] = useState<string>('');
   const [email, setEmail] = useState<string>('');
   const [message, setMessage] = useState<string>('');
-  const [errors, setErrors] = useState<Partial<User>>({});
 
   const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => setFirstName(event.target.value);
   const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => setLastName(event.target.value);
@@ -30,44 +29,7 @@ const Signup: React.FC = () => {
   const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(event.target.value);
   const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => setEmail(event.target.value);
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault(); // Prevent default form submission behavior
-
-    // Reset error message
-    setMessage('');
-
-    // Perform form validation
-    const errors: Partial<User> = {};
-    if (!firstName.trim()) {
-      errors.firstName = 'First name is required';
-    }
-    if (!lastName.trim()) {
-      errors.lastName = 'Last name is required';
-    }
-    if (!username.trim()) {
-      errors.username = 'Username is required';
-    }
-    if (!email.trim()) {
-      errors.email = 'Email is required';
-    } else if (!/^\S+@\S+\.\S+$/.test(email)) {
-      errors.email = 'Invalid email format';
-    }
-    if (!password.trim()) {
-      errors.password = 'Password is required';
-    } else if (password.length < 6) {
-      errors.password = 'Password must be at least 6 characters';
-    }
-    if (password !== confirmPassword) {
-      errors.confirmPassword = 'Passwords do not match';
-    }
-
-    // Set errors if any
-    if (Object.keys(errors).length > 0) {
-      setErrors(errors);
-      return; // Exit early if there are validation errors
-    }
-
-    // If no validation errors, proceed with form submission
+  const handleSubmit = async () => {
     try {
       const user: User = {
         username, password, confirmPassword, firstName, lastName, email,
@@ -92,30 +54,21 @@ const Signup: React.FC = () => {
           </StyledAvatar>
           <Typography variant="h5">Signup</Typography>
         </Grid>
-        <form onSubmit={handleSubmit}>
+        <form>
           <Grid container justifyContent="center" alignItems="center" direction="column">
-            <StyledTextField
-              label="First name"
-              placeholder="Enter First name"
-              fullWidth
-              required
-              value={firstName}
-              onChange={handleFirstNameChange}
-              error={!!errors.firstName}
-              helperText={errors.firstName}
-            />
-            <StyledTextField label="Last name" placeholder="Enter Last name" fullWidth required value={lastName} onChange={handleLastNameChange} error={!!errors.lastName} helperText={errors.lastName} />
-            <StyledTextField label="Username" placeholder="Enter username" fullWidth required value={username} onChange={handleUserNameChange} error={!!errors.username} helperText={errors.username} />
-            <StyledTextField label="Email" placeholder="Enter email" fullWidth required value={email} onChange={handleEmailChange} error={!!errors.email} helperText={errors.email} />
-            <StyledTextField label="Password" placeholder="Enter password" type="password" fullWidth required value={password} onChange={handlePasswordChange} error={!!errors.password} helperText={errors.password} />
-            <StyledTextField label="Confirm Password" placeholder="Confirm password" type="password" fullWidth required value={confirmPassword} onChange={handleConfirmPasswordChange} error={!!errors.confirmPassword} helperText={errors.confirmPassword} />
-            <StyledButton type="submit" fullWidth variant="contained" color="primary">
-              Sign Up
-            </StyledButton>
-            <Typography style={{ margin: 7, color: 'red' }} variant="body1">
-              {message}
-            </Typography>
+            <StyledTextField label="First name" placeholder="Enter First name" fullWidth required value={firstName} onChange={handleFirstNameChange} />
+            <StyledTextField label="Last name" placeholder="Enter Last name" fullWidth required value={lastName} onChange={handleLastNameChange} />
           </Grid>
+          <StyledTextField label="Username" placeholder="Enter username" fullWidth required value={username} onChange={handleUserNameChange} />
+          <StyledTextField label="Email" placeholder="Enter email" fullWidth required value={email} onChange={handleEmailChange} />
+          <StyledTextField label="Password" placeholder="Enter password" type="password" fullWidth required value={password} onChange={handlePasswordChange} />
+          <StyledTextField label="Confirm Password" placeholder="Confirm password" type="password" fullWidth required value={confirmPassword} onChange={handleConfirmPasswordChange} />
+          <StyledButton fullWidth variant="contained" color="primary" onClick={handleSubmit}>
+            Sign Up
+          </StyledButton>
+          <Typography style={{ margin: 7, color: 'red' }} variant="body1">
+            {message}
+          </Typography>
         </form>
         <Typography>
           Already have an account?
