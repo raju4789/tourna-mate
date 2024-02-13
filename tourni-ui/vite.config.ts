582f8b714f37f2bc5c2d77e11efa46e750fa89e4
Raju MLN <narasimha4789@gmail.com>
2024-02-13 21:30:08 +0700

    adding routing support

--- tourni-ui/vite.config.ts
+++ tourni-ui/vite.config.ts
@@ -2,16 +2,13 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { ConfigEnv, defineConfig, loadEnv } from 'vite';
+import { ConfigEnv, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv) => {
-  // Load the environment variables based on the mode
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  // Access environment variables using process.env
-  const VITE_API_URL = process.env.VITE_API_URL || 'http://localhost:8080';
-  const VITE_UI_PORT = process.env.VITE_UI_PORT ? parseInt(process.env.VITE_UI_PORT, 10) : 8001;
+  const VITE_API_URL: string = (mode !== 'production') ? 'http://localhost:8080' : 'https://tourni-gateway:8080';
+  const VITE_UI_PORT: number = (mode !== 'production') ? 8001 : 8000;
 
   console.log('VITE_API_URL:', VITE_API_URL);
   console.log('VITE_UI_PORT:', VITE_UI_PORT);
