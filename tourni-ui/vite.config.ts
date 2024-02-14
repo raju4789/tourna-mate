7f4cc8fa526d61873df28055120023389691cf4e
Raju MLN <narasimha4789@gmail.com>
2024-02-14 20:40:03 +0700

    fixed docker file issue

--- tourni-ui/vite.config.ts
+++ tourni-ui/vite.config.ts
@@ -2,30 +2,27 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
-import { ConfigEnv, defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default ({ mode }: ConfigEnv) => {
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-      port: 8000,
-      host: true,
-      strictPort: true,
-      proxy: {
-        '/api': {
-          target: 'https://tourni-gateway:8080', // The backend server
-          changeOrigin: true, // Needed for virtual hosted sites
-          secure: false, // Set to true if your backend is served over HTTPS
-        },
+export default () => defineConfig({
+  plugins: [react()],
+  server: {
+    port: 8000,
+    host: true,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'https://tourni-gateway:8080', // The backend server
+        changeOrigin: true, // Needed for virtual hosted sites
+        secure: false, // Set to true if your backend is served over HTTPS
       },
     },
-    test: {
-      globals: true,
-      environment: 'jsdom',
-      setupFiles: ['./src/setupTests.ts'],
-    },
-  });
-};
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+});
