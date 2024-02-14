c1872efc08370f5a1ce8ba08560d23e3c526c219
Raju Methuku <raju.methuku@agoda.com>
2024-02-14 18:43:38 +0700

    code standardised

--- tourni-ui/vite.config.ts
+++ tourni-ui/vite.config.ts
@@ -10,16 +10,12 @@ export default ({ mode }: ConfigEnv) => {
 
   return defineConfig({
     plugins: [react()],
-    preview: {
-      host: true,
-      port: 8000,
-    },
     server: {
       port: 8000,
       host: true,
       strictPort: true,
       proxy: {
-        '/api/v1': {
+        '/api': {
           target: 'https://tourni-gateway:8080', // The backend server
           changeOrigin: true, // Needed for virtual hosted sites
           secure: false, // Set to true if your backend is served over HTTPS
