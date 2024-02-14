769e50c34d7e928c13c57c6ff7338ead437ef20a
Raju Methuku <raju.methuku@agoda.com>
2024-02-14 15:53:51 +0700

    code standardised

--- tourni-ui/vite.config.ts
+++ tourni-ui/vite.config.ts
@@ -7,11 +7,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv) => {
-  const VITE_API_URL: string = (mode !== 'production') ? 'http://localhost:8080' : 'https://tourni-gateway:8080';
-  const VITE_UI_PORT: number = (mode !== 'production') ? 8001 : 8000;
-
-  console.log('VITE_API_URL:', VITE_API_URL);
-  console.log('VITE_UI_PORT:', VITE_UI_PORT);
 
   return defineConfig({
     plugins: [react()],
